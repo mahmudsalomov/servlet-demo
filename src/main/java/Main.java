ec6bc08dda2d86243adb584f2496afe5ba861487
kali <primat.2000@mail.ru>
2021-02-10 02:49:01 +0500

    preparedStatement

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import java.sql.SQLException;
-
-public class Main {
-    public static void main(String[] args) throws SQLException {
-        CrudService crudService=new CrudService();
-        System.out.println(crudService.getAll());
-    }
-}
